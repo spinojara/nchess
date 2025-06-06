a5dcc329c750b32b6791d2066080ed29c2b2e067
Isak Ellmer <isak01@gmail.com>
2025-06-06 23:17:16 +0200

    Add test engine

---
+++ src/nchessengine.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("id name nchessengine\n");
+	printf("id author Isak Ellmer\n");
+	printf("option name AVeryVeryLongOptionNameCheck type check default true\n");
+	printf("option name AVeryVeryLongOptionNameSpin type spin default 12831237 min 0 max 12381024890\n");
+	printf("option name AVeryVeryLongOptionNameCombo type combo var AVeryVeryLongVariantName\n");
+	printf("option name AVeryVeryVeryLongOptionNameButton type button\n");
+	printf("option name AVeryVeryLongOptionNameString type string default AVeryVeryLongString\n");
+	return 0;
+}
